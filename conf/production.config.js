121c8a6fef75d3f4feed3c62b80713713ace7991
Tom Taylor <hello@tomtaylor.name>
2024-02-29 17:28:53 +0000

    Update production.config.js

--- conf/production.config.js
+++ conf/production.config.js
@@ -2,18 +2,8 @@ export default {
   'adapt-authoring-assets': {
     maxFileSize: '500MB'
   },
-  'adapt-authoring-logger': {
-    levels: [
-      'info',
-      'warn',
-      'error',
-      'debug'
-    ]
-  },
-  'adapt-authoring-mongodblogger': {
-    maxLogCount: -1
-  },
   'adapt-authoring-server': {
+    host: '0.0.0.0',
     port: process.env.PORT,
     verboseErrorLogging: true
   }
