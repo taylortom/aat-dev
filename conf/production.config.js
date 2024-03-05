f85a8b7385eba88a6a96095e3d2b8e9a8a8377ea
Thomas Taylor <hello@tomtaylor.name>
2024-03-05 11:42:04 +0000

    Move all config items to env

--- conf/production.config.js
+++ conf/production.config.js
@@ -1,17 +1,5 @@
 export default {
-  'adapt-authoring-assets': {
-    maxFileSize: '500MB'
-  },
-  'adapt-authoring-core': {
-    dataDir: '/home/ADAPT/data',
-    tempDir: '/home/ADAPT/temp'
-  },
-  'adapt-authoring-localauth': {
-    initialSuperUserEmail: 'dev@kineo.com'
-  },
   'adapt-authoring-server': {
-    host: '0.0.0.0',
-    port: process.env.PORT,
-    verboseErrorLogging: true
+    port: process.env.PORT
   }
 }
